a9681f452977b925df2e4b2b3a98c12dcec5b5c8
ivert <ivertfiks42@gmail.com>
2025-09-06 17:04:08 +0200

    Changes by team

--- src/main/java/com/todolist/todo/DTO/GenericError.java
+++ src/main/java/com/todolist/todo/DTO/GenericError.java
@@ -1,10 +1,14 @@
 package com.todolist.todo.DTO;
 
+import lombok.Data;
 import java.time.LocalDateTime;
+import java.util.Map;
 
+@Data
 public class GenericError {
 
-    private LocalDateTime localDateTime;
-    private String message;
+    private final LocalDateTime localDateTime;
+    private final String path;
+    private final Map<String, String> errors;
 
 }
--- src/main/java/com/todolist/todo/handler/GlobalExceptionHandler.java
+++ src/main/java/com/todolist/todo/handler/GlobalExceptionHandler.java
@@ -1,22 +1,32 @@
 package com.todolist.todo.handler;
 
 import com.todolist.todo.DTO.GenericError;
-import jakarta.validation.ConstraintViolationException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<GenericError> handleConstraintViolation(ConstraintViolationException ex) {
-        LocalDateTime localDateTime = LocalDateTime.now();
-
-        // todo create and return ResponseEntity<GenericError>
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<GenericError> handleValidationExceptions(MethodArgumentNotValidException ex,
+                                                                          HttpServletRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        var errorDto = new GenericError(LocalDateTime.now(), request.getRequestURI(), errors);
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
     }
 
 }
